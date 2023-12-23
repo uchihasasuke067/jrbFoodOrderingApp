6c2c191964d2cd84809c3b0d9af198e091ecd842
Jay Bautista <jaybautista@Jays-MacBook-Air.local>
2023-12-23 11:41:12 +0800

    user async work

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,10 @@
-export function POST(req){
-    return Response.json('ok');
+import {User} from "../models/User";
+import mongoose from "mongoose";
+
+export async function POST(req){
+    const body = await req.json();
+
+    mongoose.connect(process.env.MONGO_URL);
+    const createdUser = await User.create(body);
+    return Response.json(createdUser);
 }
