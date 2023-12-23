942cb4d598b9651ade87b33f7f8bcf032157620c
Jay Bautista <jaybautista@Jays-MacBook-Air.local>
2023-12-23 13:31:12 +0800

    registered account working with mongodb

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -8,9 +8,16 @@ const UserSchema = new Schema({
         validate: pass => {
             if(!pass?.length || pass.length < 5){
                 new Error('password must be at least 5 characters');
+                return false;
             }
         },
     },
 }, {timestamps: true});
+
+// UserSchema.post('validate', function(user){
+// const notHashedPassword = user.password;
+// const salt = bcrypt.genSaltSync(10);
+// user.password = bcrypt.HashSync(notHashedPassword, salt);
+// })
 
 export const User = models?.User || model('User', UserSchema);
