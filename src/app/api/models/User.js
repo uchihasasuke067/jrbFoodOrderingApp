e63deac178652dae89a4ac3cbbb2e448e5400fbe
Jay Bautista <jaybautista@Jays-MacBook-Air.local>
2023-12-26 21:23:17 +0800

    reg done with encryption

--- src/app/api/models/User.js
+++ src/app/api/models/User.js
@@ -1,23 +1,35 @@
-import { model, models, Schema } from "mongoose";
+import crypto from 'crypto';
+import { model, models, Schema } from 'mongoose';
 
 const UserSchema = new Schema({
-    email: {type: String, required: true, unique: true},
-    password:{
-        type: String,
-        required: true,
-        validate: pass => {
-            if(!pass?.length || pass.length < 5){
-                new Error('password must be at least 5 characters');
-                return false;
-            }
-        },
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    validate: {
+      validator: pass => {
+        return pass && pass.length >= 5;
+      },
+      message: 'Password must be at least 5 characters long',
     },
-}, {timestamps: true});
+  },
+  salt: String, // Store the salt for each user
+}, { timestamps: true });
 
-// UserSchema.post('validate', function(user){
-// const notHashedPassword = user.password;
-// const salt = bcrypt.genSaltSync(10);
-// user.password = bcrypt.HashSync(notHashedPassword, salt);
-// })
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.createHmac('sha256', salt)
+    .update(user.password)
+    .digest('hex');
+
+  user.password = hash; // Store the encrypted password in 'password'
+  user.salt = salt;
+  next();
+});
 
 export const User = models?.User || model('User', UserSchema);
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,44 +1,73 @@
 'use client';
 import React, { useState } from 'react'
-import Image from 'next/image'
+import Link from 'next/link';
 
 function RegisterPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [creatingUser, setCreatingUser]=useState(false);
+  const [userCreated, setUserCreated]=useState(false);
+  const [error, setError] =useState(false);
 
-  function handleFormSubmit(ev){
+  async function handleFormSubmit(ev){
     ev.preventDefault();
-    fetch('/api/register',{
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers:{'Content-Type': 'application/json'},
-    });
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+      const response = await fetch('/api/register',{
+        method: 'POST',
+        body: JSON.stringify({email, password}),
+        headers:{'Content-Type': 'application/json'},
+      });
+
+      if (response.ok){
+        setUserCreated(true);
+      }
+      else {
+        setError(true);
+      }
+      setCreatingUser(false);
   }
   return (
     <section className='mt-4'>
         <h1 className='text-center text-primary text-4xl'>
             Register
         </h1>
+        {userCreated && (
+          <div className='my-4 text-center'>
+            User created. <br/>
+            Now you can {' '}
+            <Link href={'/login'} className='underline'> login &rarr;</Link>
+          </div>
+        )}
+        {error && (
+        <div className='my-4 text-center text-red-600 text-semibold'>
+          User not created. <br/>
+          An Error has been encounter. Try again later
+          </div>
+        )}
         <form className='block max-w-xs mx-auto mt-4' onSubmit={handleFormSubmit}>
-            <input type="email" placeholder='Email' value={email}
+            <input name="email" type="email" placeholder='Email' value={email}
+            disabled={creatingUser}
             onChange={ev => setEmail(ev.target.value)}/>
-            <input type="password" placeholder='Password' value={password}
+            <input name="pwd" type="password" placeholder='Password' value={password}
+            disabled={creatingUser}
             onChange={ev => setPassword(ev.target.value)}/>
-            <button type='submit'>Submit</button>
+            <button type='submit' disabled={creatingUser}>Submit</button>
             <div className='my-4 text-center text-gray-500'>
-                or use Google Account
+                or use 
             </div>
             <div className="flex space-x-4">
+            <button className="flex items-center flex-grow text-gray-600 px-3 py-1 rounded-lg">
+              <img src="/google.png" alt="Google Logo" width={20} height={20} className="mr-2" />
+              Google
+              </button>
       <button className="flex items-center flex-grow text-gray-600 px-3 py-1 rounded-lg">
-        <Image src={'/google.png'} alt={''} width={20} height={20} className="mr-2" />
-        Google
-      </button>
-      <button className="flex items-center flex-grow text-gray-600 px-3 py-1 rounded-lg">
-        <Image src={'/fb.png'} alt={''} width={20} height={20} className="mr-2" />
+        <img src={'/fb.png'} alt={''} width={20} height={20} className="mr-2" />
         Facebook
       </button>
       <button className="flex items-center flex-grow text-gray-600 px-3 py-1 rounded-lg">
-        <Image src={'/github.png'} alt={''} width={20} height={20} className="mr-2" />
+        <img src={'/github.png'} alt={''} width={20} height={20} className="mr-2" />
         Github
       </button>
     </div>
